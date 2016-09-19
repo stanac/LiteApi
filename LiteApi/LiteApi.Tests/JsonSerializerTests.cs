1b73774d4187b131ab573aef6bb802cf54a31f1d
Milan Stanacev <milan@stanacev.com>
2016-09-19 11:32:54 +0200

    json serializer test

---
+++ LiteApi/LiteApi.Tests/JsonSerializerTests.cs
@@ -0,0 +1,28 @@
+﻿using LiteApi.Services;
+using Xunit;
+
+namespace LiteApi.Tests
+{
+    public class JsonSerializerTests
+    {
+        [Fact]
+        public void DefaultJsonSerializer_CanSeralizeAndDeserailize()
+        {
+            JsonSerializer s = new JsonSerializer();
+            var model = new TestModel();
+            string json = s.Serialize(model);
+            TestModel obj1 = s.Deserialize<TestModel>(json);
+            TestModel obj2 = (TestModel)s.Deserialize(json, typeof(TestModel));
+            Assert.Equal(model.I, obj1.I);
+            Assert.Equal(model.J, obj1.J);
+            Assert.Equal(model.I, obj2.I);
+            Assert.Equal(model.J, obj2.J);
+        }
+
+        private class TestModel
+        {
+            public int I { get; set; } = 3;
+            public string J { get; set; } = "Test";
+        }
+    }
+}
