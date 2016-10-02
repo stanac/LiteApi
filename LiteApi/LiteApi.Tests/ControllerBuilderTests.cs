6e9f4684b02fcb8c1d5990698d88982e608c7cb7
Milan Stanacev <milan@stanacev.com>
2016-10-02 12:21:23 +0200

    constructor builder test

---
+++ LiteApi/LiteApi.Tests/ControllerBuilderTests.cs
@@ -0,0 +1,16 @@
+﻿using LiteApi.Services;
+using Xunit;
+
+namespace LiteApi.Tests
+{
+    public class ControllerBuilderTests
+    {
+        [Fact]
+        public void ControllerBuilder_ControllerWithMultipleConstructors_WillUseOneMarkedWithApiContructorAttr()
+        {
+            var ctrlBuilder = new ControllerBuilder();
+            var ctrl = ctrlBuilder.BuildObject<Controllers.BuilderTestController>();
+            Assert.Equal("default", ctrl.StrVal);
+        }
+    }
+}
---
+++ LiteApi/LiteApi.Tests/Controllers/BuilderTestController.cs
@@ -0,0 +1,20 @@
+﻿using LiteApi.Attributes;
+
+namespace LiteApi.Tests.Controllers
+{
+    public class BuilderTestController : LiteController
+    {
+        public string StrVal { get; set; }
+
+        [ApiConstructor]
+        public BuilderTestController()
+        {
+            StrVal = "default";
+        }
+
+        public BuilderTestController(string isSet)
+        {
+            StrVal = isSet;
+        }
+    }
+}
