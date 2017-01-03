7108eb53d44b5614375bab42d0a61263118f553e
Milan Stanacev <milan@stanacev.com>
2017-01-03 15:15:16 +0100

    app builder extensions tests

---
+++ LiteApi/LiteApi.Tests/Fakes/FakeApplicationBuilder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace LiteApi.Tests.Fakes
+{
+    public class FakeApplicationBuilder : IApplicationBuilder
+    {
+        public bool CalledUse { get; private set; }
+
+        public IServiceProvider ApplicationServices { get; set; }
+
+        public IDictionary<string, object> Properties { get; set; }
+
+        public IFeatureCollection ServerFeatures
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public RequestDelegate Build()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IApplicationBuilder New()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IApplicationBuilder Use(Func<RequestDelegate, RequestDelegate> middleware)
+        {
+            CalledUse = true;
+            return this;
+        }
+    }
+}
---
+++ LiteApi/LiteApi.Tests/IApplicationBuilderExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Builder;
+
+namespace LiteApi.Tests
+{
+    public class IApplicationBuilderExtensionsTests
+    {
+        [Fact]
+        public void ApplicationBuilderExtension_RegisterMiddleware_CallsRegistraionOfApplicationBuilder()
+        {
+            var builder = new Fakes.FakeApplicationBuilder();
+            ApplicationBuilderExtenstions.UseLiteApi(builder);
+            Assert.True(builder.CalledUse);
+        }
+    }
+}
