041aa1b48596f969f7547b11e4ca414c036c902e
Milan Stanacev <milan@stanacev.com>
2017-01-04 17:15:00 +0100

    additional data tests

---
+++ LiteApi/LiteApi.Tests/AdditionalDataTests.cs
@@ -0,0 +1,29 @@
+﻿using LiteApi.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LiteApi.Tests
+{
+    public class AdditionalDataTests
+    {
+        [Fact]
+        public void AdditionalData_SetAdditionalData_CanGetAdditionalData()
+        {
+            var ad = new AdditionalData();
+            ad.SetAdditionalData("1", 11);
+            int setData = ad.GetAdditionalDataOrDefault<int>("1");
+            Assert.Equal(11, setData);
+        }
+
+        [Fact]
+        public void AdditionalData_NotAdditionalData_CanGetDefaultValue()
+        {
+            var ad = new AdditionalData();
+            int setData = ad.GetAdditionalDataOrDefault<int>("1");
+            Assert.Equal(default(int), setData);
+        }
+    }
+}
