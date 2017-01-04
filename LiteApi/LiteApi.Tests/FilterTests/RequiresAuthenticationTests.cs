674258c39a3600e07e8803fb136cb057039562fd
Milan Stanacev <milan@stanacev.com>
2017-01-04 16:17:35 +0100

    RequiresAuthenticationTests

---
+++ LiteApi/LiteApi.Tests/FilterTests/RequiresAuthenticationTests.cs
@@ -0,0 +1,43 @@
+﻿using LiteApi.Attributes;
+using LiteApi.Contracts.Abstractions;
+using LiteApi.Tests.ModelSetup;
+using System.Security.Claims;
+using Xunit;
+
+namespace LiteApi.Tests.FilterTests
+{
+    public class RequiresAuthenticationTests
+    {
+        [Fact]
+        public void RequiresAuthentication_UnauthenticatedUser_ReturnsUnauthenticated()
+        {
+            var user = new ClaimsPrincipal();
+            var attr = new RequiresAuthenticationAttribute();
+            var httpCtx = new Fakes.FakeHttpContext();
+            httpCtx.User = user;
+            var shouldContinue = attr.ShouldContinue(httpCtx);
+            Assert.False(shouldContinue.ShouldContinue);
+            Assert.Equal(ApiFilterRunResult.Unauthenticated.SetResponseCode, shouldContinue.SetResponseCode);
+        }
+
+        [Fact]
+        public void RequiresAuthentication_NullUser_ReturnsUnauthenticated()
+        {
+            var attr = new RequiresAuthenticationAttribute();
+            var httpCtx = new Fakes.FakeHttpContext();
+            var shouldContinue = attr.ShouldContinue(httpCtx);
+            Assert.False(shouldContinue.ShouldContinue);
+            Assert.Equal(ApiFilterRunResult.Unauthenticated.SetResponseCode, shouldContinue.SetResponseCode);
+        }
+
+        [Fact]
+        public void RequiresAuthentication_AuthenticatedUser_ReturnsContinue()
+        {
+            var attr = new RequiresAuthenticationAttribute();
+            var httpCtx = new Fakes.FakeHttpContext();
+            httpCtx.User = UserSetup.GetUser();
+            var shouldContinue = attr.ShouldContinue(httpCtx);
+            Assert.True(shouldContinue.ShouldContinue);
+        }
+    }
+}
