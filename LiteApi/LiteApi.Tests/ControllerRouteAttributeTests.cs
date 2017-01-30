9f3149ff95aae66547ba74d3d3b0782c7526764e
Milan Stanacev <milan@stanacev.com>
2017-01-30 19:55:12 +0100

    route attribute tests

---
+++ LiteApi/LiteApi.Tests/ControllerRouteAttributeTests.cs
@@ -0,0 +1,44 @@
+﻿using LiteApi.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LiteApi.Tests
+{
+    public class ControllerRouteAttributeTests
+    {
+        [Fact]
+        public void ControllerRouteAttribute_BackslashesAreReplaced()
+        {
+            var attrib = new ControllerRouteAttribute("a/b\\c\\d");
+            int backslashCount = attrib.Route.Count(x => x == '\\');
+            Assert.Equal(0, backslashCount);
+        }
+        
+        [Fact]
+        public void ControllerRouteAttribute_SlashesAreTrimmed()
+        {
+            var attrib = new ControllerRouteAttribute("/a/b/c/d/");
+            
+            Assert.False(attrib.Route.StartsWith("/", StringComparison.Ordinal));
+            Assert.False(attrib.Route.EndsWith("/", StringComparison.Ordinal));
+        }
+        
+        [Fact]
+        public void ControllerRouteAttribute_NullName_ThrowsException()
+        {
+            bool error = false;
+            try
+            {
+                var a = new ControllerRouteAttribute(null);
+            }
+            catch (ArgumentNullException)
+            {
+                error = true;
+            }
+            Assert.True(error);
+        }
+    }
+}
